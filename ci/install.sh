2c5eb736139982ca4a1a4287ade346796020005a
Aaron Hill <aa1ronham@gmail.com>
2019-07-29 21:11:35 -0400

    Try again

--- ci/install.sh
+++ ci/install.sh
@@ -1,3 +1,3 @@
 sudo apt-get install vagrant qemu libvirt-bin ebtables dnsmasq-base
-sudo apt-get install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby
+sudo apt-get install libxslt-dev libxml2-dev zlib1g-dev ruby
 vagrant plugin install vagrant-libvirt
