54e8c87be01b3af890987338f3fb7a7d620d3b2d
Aaron Hill <aa1ronham@gmail.com>
2019-07-29 21:19:21 -0400

    Install libvirt-dev

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install qemu libvirt-bin ebtables dnsmasq-base
+sudo apt-get install qemu libvirt-dev libvirt-bin ebtables dnsmasq-base
 sudo apt-get install libxslt-dev libxml2-dev zlib1g-dev ruby
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
 sudo dpkg -i vagrant_2.2.5_x86_64.deb
