24be9ab32ebf1676d73e87a2c6be4be606c7228f
Aaron Hill <aa1ronham@gmail.com>
2019-07-29 21:07:19 -0400

    Initial commit

---
+++ ci/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get build-dep vagrant ruby-libvirt
+sudo apt-get install qemu libvirt-bin ebtables dnsmasq-base
+sudo apt-get install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
+vagrant plugin install vagrant-libvirt
