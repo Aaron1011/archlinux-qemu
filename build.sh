5d1accd1626ebf050839085b5d2b393be19f4bc9
Aaron Hill <aa1ronham@gmail.com>
2019-08-10 15:55:49 -0400

    Make progress to gettting Vagrant working

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+set -xe
 docker-compose build
 docker-compose up
-docker commit -c 'ENTRYPOINT ["do_run.sh"]' -c 'CMD ["/bin/bash"]' ubuntu-vagrant final-ubuntu-vagrant
+docker commit -c 'ENTRYPOINT ["/do_run.sh"]' -c 'CMD ["/bin/bash"]' ubuntu-vagrant final-ubuntu-vagrant
--- do_vagrant_build.sh
+++ do_vagrant_build.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
-set -e
+set -xe
 apt-get install iproute2
 ip addr
 ./start_libvirt.sh
 
+vagrant box update
 vagrant up --provider=libvirt
+vagrant ssh -c "sudo pacman -S --noconfirm git"
+vagrant ssh -c "git clone https://aur.archlinux.org/yay.git"
+vagrant ssh -c "cd yay"
+vagrant ssh -c "makepkg -si"
+vagrant ssh -c "yay -S --noconfirm linux-mainline"
+
 # 'Nuclear option' from https://forum.manjaro.org/t/issues-with-signature-is-marginal-trust-or-invalid-or-corrupted-package/33268/5
 # Workaround for https://github.com/archlinux/arch-boxes/issues/41
-vagrant ssh -c "sudo rm -fr /etc/pacman.d/gnupg"
-vagrant ssh -c "sudo pacman-key --init"
-vagrant ssh -c "sudo pacman-key --populate archlinux"
-vagrant ssh -c "sudo pacman-key --refresh-keys"
-vagrant reload
-vagrant rsync
+#vagrant ssh -c "sudo rm -fr /etc/pacman.d/gnupg"
+#vagrant ssh -c "sudo pacman-key --init"
+#vagrant ssh -c "sudo pacman-key --populate archlinux"
+#vagrant ssh -c "sudo pacman-key --refresh-keys"
+#vagrant reload
+#vagrant rsync
