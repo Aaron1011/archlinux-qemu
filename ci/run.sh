4d7343020782d8d68ad8b33f783aada8505d2121
Aaron Hill <aa1ronham@gmail.com>
2019-09-26 20:27:39 -0400

    Use built container on ci

--- ci/run.sh
+++ ci/run.sh
@@ -1,16 +1,20 @@
-sudo chown -R 777 /var/run/libvirt/ # It's CI - who cares about perms?
-newgrp libvirt
+set -xe
+docker pull aaron1011/arch-docker
+docker run --privileged -it final-ubuntu-vagrant 'uname -a'
 
-sudo vagrant up --provider=libvirt
+#sudo chown -R 777 /var/run/libvirt/ # It's CI - who cares about perms?
+#newgrp libvirt
+
+#sudo vagrant up --provider=libvirt
 
 # 'Nuclear option' from https://forum.manjaro.org/t/issues-with-signature-is-marginal-trust-or-invalid-or-corrupted-package/33268/5
 # Workaround for https://github.com/archlinux/arch-boxes/issues/41
-sudo vagrant ssh -c "sudo rm -fr /etc/pacman.d/gnupg"
-sudo vagrant ssh -c "sudo pacman-key --init"
-sudo vagrant ssh -c "sudo pacman-key --populate archlinux"
-sudo vagrant ssh -c "sudo pacman-key --refresh-keys"
-sudo vagrant reload
+#sudo vagrant ssh -c "sudo rm -fr /etc/pacman.d/gnupg"
+#sudo vagrant ssh -c "sudo pacman-key --init"
+#sudo vagrant ssh -c "sudo pacman-key --populate archlinux"
+#sudo vagrant ssh -c "sudo pacman-key --refresh-keys"
+#sudo vagrant reload
 
-sudo vagrant rsync
-sudo vagrant ssh -c "ls -l /vagrant"
-sudo vagrant ssh -c "uname -a"
+#sudo vagrant rsync
+#sudo vagrant ssh -c "ls -l /vagrant"
+#sudo vagrant ssh -c "uname -a"
